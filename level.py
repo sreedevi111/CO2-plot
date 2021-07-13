850e0ca97c89463c076c2cfa4d1c03b77aea51c4
sreedevi <sreedevisobhana@gmail.com>
2021-07-13 15:02:23 +0530

    initial commit

---
+++ level.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import xarray as xr
+import cartopy.crs as ccrs
+ds = xr.open_mfdataset('*_summer_*.nc')
+co2 = ds.co2.mean('time')
+co2graph = co2.isel(level=0)
+ax = plt.axes(projection=ccrs.PlateCarree())
+#ax = co2graph.plot( cmap='jet', extend='max', vmax=440, vmin=390))
+ax.coastlines()
+co2graph.plot(cmap='jet',vmax=440, vmin=390)
+plt.title('summer', pad=1)
+# plt.xlabel('latitude')
+# plt.ylabel('longitude')
+plt.show()
